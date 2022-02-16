eb23ebc0308e83e2699c20872ddcd719ef331a60
Lord Baryhobal <lordbaryhobal@gmail.com>
2022-02-16 22:33:16 +0100

    basic game

---
+++ 02_Amazing/amazing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import curses
+import numpy as np
+
+def init(stdscr):
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+
+def end(stdscr):
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+def generate_maze(width=20,height=20):
+    maze = np.ones([2*height+1, 2*width+1])
+
+    
+
+    return maze
+
+def draw(maze):
+
+if __name__ == '__main__':
+    stdscr = curses.initscr()
+    init(stdscr)
+
+    maze = generate_maze()
+
+    while True:
+        draw(maze)
+
+    end(stdscr)
